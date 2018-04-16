21408e22a576c6b671f19c157c6b4baaa8a64a28
MinhThanh <MinhThanh@MinhThanh-macbook.local>
2018-04-16 17:08:00 +0700

    inital commit

--- session3/homework/sheep26.py
+++ session3/homework/sheep26.py
@@ -12,9 +12,32 @@ size.insert(size.index(max),8)
 size.remove (max)
 print ("After shearing, here is my flock", end="\n")
 print (size)
-#grow
+#month_nhap_so_thang
+for m in range (2):
+    print ("Month", m+1)
+    for i in size:
+        i2=i+50
+        size.insert(size.index(i),i2)
+        size.remove (i)
+    print ("One month has passed, here is my flock", size, sep="\n")
+    #choose the biggest
+    max = size[0]
+    for i in size:
+        if i> max:
+            max = i
+    print ("Now my biggest sheeps has size", max, "let's shear it")
+    #change size
+    size.insert(size.index(max),8)
+    size.remove (max)
+    print ("After shearing, here is my flock", end="\n")
+    print (size)
+print ("Month 3")
 for i in size:
     i2=i+50
     size.insert(size.index(i),i2)
     size.remove (i)
 print ("One month has passed, here is my flock", size, sep="\n")
+totalsize = sum(size)
+print ('My flock has size in total:', totalsize)
+totalmoney = totalsize*2
+print ("I would get ", totalsize," * 2$ = ", totalmoney, "$", sep='')
